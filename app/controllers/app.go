PacketFire/goqdb

commit 7a03f3f59e88e29247094546e4b577266aba9d64

basic fix for #6

app/controllers/app.go
@@ -66,6 +66,14 @@ func loadEntries(results []interface{}, err error) []*models.QdbEntry {
 func (c App) Post() revel.Result {
 	var quote models.QdbEntry
 
+	quote.Validate(c.Validation)
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(routes.App.Index("", 0, 0))
+	}
+
 	quote.Created = time.Now().Unix()
 	quote.Rating = 0
 

app/models/qdb.go
@@ -2,7 +2,7 @@ package models
 
 import (
 	//        "github.com/coopernurse/gorp"
-	//        "github.com/robfig/revel"
+	        "github.com/robfig/revel"
 	"time"
 	"strings"
 )
@@ -32,4 +32,9 @@ func (q *QdbEntry) Clip() string {
 
 func (q *QdbEntry) Time() string {
 	return time.Unix(q.Created, 0).Format(time.UnixDate)
+}
+
+func (q *QdbEntry) Validate(v *revel.Validation) {
+	v.Required(q.Quote)
+	v.Required(q.Author)
 }

app/views/App/Index.html
@@ -4,6 +4,18 @@
 <div id="main">
 	<a href="/"><h3>QDB</h3></a>
 	<br/>
+
+	{{if .errors}}
+	<div>
+		<h4>Error</h4>
+		<ul>
+		{{range .errors}}
+			<li>{{.}}</li>
+		{{end}}
+		</ul>
+	</div>
+	{{end}}
+
 	<form role="form" method="post" action="/">
 		<!--
 		<legend>quote</legend>

commit 404b75c43aea6520fa6517e2e213c3e692fefa79

fixes #6 and removes errors introduced in 7a03f3f59e

app/controllers/app.go
@@ -66,19 +66,20 @@ func loadEntries(results []interface{}, err error) []*models.QdbEntry {
 func (c App) Post() revel.Result {
 	var quote models.QdbEntry
 
-	quote.Validate(c.Validation)
+	quote.Created = time.Now().Unix()
+	quote.Rating = 0
+
+	c.Params.Bind(&quote.Author, "author")
+	c.Params.Bind(&quote.Quote, "quote")
+
+	c.Validation.Required(quote.Author)
+	c.Validation.Required(quote.Quote)
 
 	if c.Validation.HasErrors() {
 		c.Validation.Keep()
 		c.FlashParams()
 		return c.Redirect(routes.App.Index("", 0, 0))
 	}
-
-	quote.Created = time.Now().Unix()
-	quote.Rating = 0
-
-	c.Params.Bind(&quote.Author, "author")
-	c.Params.Bind(&quote.Quote, "quote")
 	c.Txn.Insert(&quote)
 	return c.Redirect(routes.App.Index("", 0, 0))
 }

app/models/qdb.go
@@ -2,7 +2,7 @@ package models
 
 import (
 	//        "github.com/coopernurse/gorp"
-	        "github.com/robfig/revel"
+	//        "github.com/robfig/revel"
 	"time"
 	"strings"
 )
@@ -34,7 +34,3 @@ func (q *QdbEntry) Time() string {
 	return time.Unix(q.Created, 0).Format(time.UnixDate)
 }
 
-func (q *QdbEntry) Validate(v *revel.Validation) {
-	v.Required(q.Quote)
-	v.Required(q.Author)
-}